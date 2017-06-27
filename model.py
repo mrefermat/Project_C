b9f9f622464104520a4cc70dc822bcb62605edb6
Mark <mrefermat@hotmail.com>
2017-06-27 08:09:12 +0100

    Multiple look backs test and email to be sent out

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from marketdata import *
+from analytics import *
 import math
 
 def tsmom_daily(data,signal_lookback,vol_lookback=20):
@@ -42,3 +43,21 @@ def ewma_mom_daily_signal(data,short_lookback,long_lookback,vol_lookback=20):
 def calc_zscore(signal,lookback):
     return (signal/pd.ewmstd(signal,lookback,min_periods=lookback*2))
 
+
+# Equal weight of two lookbacks. Internal method for faster execution
+# since get current prices take a while to run
+def _get_positions_two_lookbacks(sh,lg,mkts,df,FundAUM,curr_px):
+    pos=tsmom_daily(df[mkts],sh)
+    pnl_short=calc_pnl_wc(pos,df[mkts])
+    pos=tsmom_daily(df[mkts],lg)
+    pnl_long=calc_pnl_wc(pos,df[mkts])
+    combined_pnl=(pnl_short*.5+pnl_long*.5)
+    scaling_factor=calc_scaling_factor(combined_pnl)
+    s_short=tsmom_daily_signal(df,sh)
+    s_long=tsmom_daily_signal(df,lg)
+    s_combined = (s_short*.5+s_long*.5)
+    return calc_position(s_combined,FundAUM,scaling_factor,curr_px)
+    
+def calc_positions_two_lookbacks(sh,lg,mkts,df,FundAUM):
+    curr_px=get_most_liquid_price(mkts)
+    return _get_positions_two_lookbacks(sh,lg,mkts,df,FundAUM,curr_px)
